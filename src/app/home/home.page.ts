6834ed87502195a1a07eb0340c5aaa44c6340913
Felix O <felix.o.stack@gmail.com>
2019-06-11 00:52:16 -0700

    Need to replace Camera logic with app permissions logic in home screen

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,44 @@
 import { Component } from '@angular/core';
+import { CameraPreview, CameraPreviewPictureOptions, CameraPreviewOptions, CameraPreviewDimensions } from '@ionic-native/camera-preview/ngx';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
+  providers: [
+    CameraPreview
+  ]
 })
 export class HomePage {
 
-  constructor() {}
+  picture: any;
+
+  constructor(
+  private cameraPreview: CameraPreview,
+  private navCtrl: NavController
+  ) {
+    const cameraPreviewOpts: CameraPreviewOptions = {
+      x: 0,
+      y: 0,
+      width: window.screen.width,
+      height: window.screen.height,
+      camera: 'rear',
+      tapPhoto: false,
+      previewDrag: false,
+      toBack: true,
+      alpha: 1
+    }
+
+    this.cameraPreview.startCamera(cameraPreviewOpts).then(
+      (res) => {
+        console.log(res)
+        this.cameraPreview.hide();
+        this.navCtrl.navigateForward('/story');
+      },
+      (err) => {
+        console.log(err)
+      });
+  }
 
 }
